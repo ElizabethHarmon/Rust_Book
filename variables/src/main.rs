18cedec18826cd99698a8598add6230aff3c4bf0
Elizabeth Harmon <betholiviaharmon@gmail.com>
2022-06-26 23:27:55 +0100

    chapter 3 part one

---
+++ variables/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    // const MAX_POINTS: u32 = 100_000;
+
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    let y = 5;
+    let y = y + 1;
+    let y = y * 2;
+    println!("The value of y is: {}", y);
+
+    let spaces = "     ";
+    let spaces = spaces.len();
+
+    println!("The number of spaces is {}", spaces);
+}
