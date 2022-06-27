294fe5177e9f90dfa7ac7f040d532358f81b6814
Elizabeth Harmon <betholiviaharmon@gmail.com>
2022-06-27 09:46:12 +0100

    finish functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -18,9 +18,25 @@
 
 fn main() {
     another_function(5, 6);
+
+    let z = seven();
+    println!("The value of z is: {}", z);
+
+    let a = plus_one(10);
+    println!("The number is {}", a);
 }
 
 fn another_function(x: i32, y: i32) {
     println!("The value of x is: {}", x);
     println!("The value of y is: {}", y);
+}
+
+// function with return
+fn seven() -> i32 {
+    7
+}
+
+// function with argument and return
+fn plus_one(a: i32) -> i32 {
+    a + 1
 }
