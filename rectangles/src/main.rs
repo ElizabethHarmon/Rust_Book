69c9281f48d46486eda470b419edaeec12d078ca
Elizabeth Harmon <betholiviaharmon@gmail.com>
2022-07-02 14:30:38 +0100

    Structs part 1

---
+++ rectangles/src/main.rs
@@ -0,0 +1,51 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+fn main() {
+    // let width1 = 30;
+    // let height1 = 50;
+
+    // println!(
+    //     "The area of the rectangle is {} square pixels",
+    //     area(width1, height1)
+
+    // let rect1 = (30, 50);
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("rect1 is {:#?}", rect1);
+
+    // println!(
+    //     "The area of the rectangle is {} square pixels",
+    //     area(rect1)
+    // );
+
+    println!(
+        "The area of the rectangle is {} square pixels",
+        rect1.area()
+    );
+}
+
+// fn area(width: u32, height: u32) -> u32 {
+//     width * height
+// }
+
+// fn area(dimensions: (u32, u32)) -> u32 {
+//     dimensions.0 * dimensions.1
+// }
+
+// fn area(rectangle: &Rectangle) -> u32 {
+//     rectangle.height * rectangle.width
+// }
