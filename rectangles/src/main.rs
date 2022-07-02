55f5475de2fd537c4b48a7d10008a01eeffdde7d
Elizabeth Harmon <betholiviaharmon@gmail.com>
2022-07-02 23:13:20 +0100

    Complete chapter 5

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -10,6 +10,14 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
 }
 
 fn main() {
@@ -27,17 +35,37 @@ fn main() {
         height: 50,
     };
 
-    println!("rect1 is {:#?}", rect1);
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("rect1 area = {} square pixels", rect1.area());
+    println!("rect2 area = {} square pixels", rect2.area());
+    println!("rect3 area = {} square pixels", rect3.area());
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let rect4 = Rectangle::square(34);
+    println!("rect4 area = {} square pixels", rect4.area());
+
+    // println!("rect1 is {:#?}", rect1);
 
     // println!(
     //     "The area of the rectangle is {} square pixels",
     //     area(rect1)
     // );
 
-    println!(
-        "The area of the rectangle is {} square pixels",
-        rect1.area()
-    );
+    // println!(
+    //     "The area of the rectangle is {} square pixels",
+    //     rect1.area()
+    // );
 }
 
 // fn area(width: u32, height: u32) -> u32 {
